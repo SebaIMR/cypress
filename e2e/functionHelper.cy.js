81c811f22f80dd2b8c44496e7db5431d33712a8e
seba <sebastianfv@live.com>
2023-12-12 15:20:20 -0300

    asd

--- e2e/functionHelper.cy.js
+++ e2e/functionHelper.cy.js
@@ -12,3 +12,28 @@ export function login(email, password) {
   
     cy.get(':nth-child(3) > .m-2').click();
   }
+
+export function crearSolicitud(){
+  cy.get('[href="#/solicitud_form"]',{ timeout: 10000 })
+    .click();
+  cy.contains('Sin detalle de valores').click();
+  //Numero de solicitud
+  cy.get('.gap-x-4 > :nth-child(1) > :nth-child(1) > .flex-col > [style="border-color: rgb(255, 51, 51);"] > .w-full')
+    .click()
+    .type('SolicitudDeAutomatizacion');
+  //Contrato
+  cy.get(':nth-child(1) > :nth-child(2) > .flex-row > .flex-col > .h-6').select('Contrato normal');
+  //informacion de contacto
+  cy.get('.gap-x-4 > :nth-child(2) > .flex-col > .mt-1 > .flex-row > .w-full')
+    .click()
+    .type('+56950441482');
+  //Hora de inicio
+  cy.get('.react-datepicker__input-container > .w-full')
+    .click();
+  cy.get('.react-datepicker__time-list > :nth-child(30)').click();
+  //Carga
+  cy.get('.css-1xc3v61-indicatorContainer')
+    .contains('SEMILLAS')
+    
+  //Descarga
+}
--- e2e/login.cy.js
+++ e2e/login.cy.js
@@ -1,7 +1,8 @@
-import { login } from './functionHelper.cy.js';
+import { login ,crearSolicitud} from './functionHelper.cy.js';
 
 describe('Login in subcargo', () => {
   it('passes', () => {
-    login('15647643','PadreAusente2023')
+    login('588438300','123456')
+    crearSolicitud()
   });
 });
